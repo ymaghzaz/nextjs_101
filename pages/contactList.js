6ec195dec268cf08c0a4618659f2d2de9c5e796a
Walid KINI <walidkini99@gmail.com>
2021-06-17 01:14:45 +0200

    using hooks

--- pages/contactList.js
+++ pages/contactList.js
@@ -1,56 +1,55 @@
 import Head from 'next/head'
 import Image from 'next/image'
 // import 'tailwindcss/tailwind.css'
-import React from "react";
+import React, { useState } from "react";
 import Client from "../comps/Client";
 import ClientForm from "../comps/ClientForm";
 
-class ClientList extends React.Component {
-  state = {
+const ClientList = () => {
+
+  const [state,setState] = useState({
     clients: [
       { id: 1, name: "Walid KINI", number: "0629279948" },
       { id: 2, name: "Youssef MAGHZAZ", number: "0618457629" },
       { id: 3, name: "Mohammed OMARI", number: "0635291847" }
     ]
-  };
+  })
 
-  handleDelete = id => {
-    const clients = [...this.state.clients];
+  const handleDelete = id => {
+    const clients = [...state.clients];
     const index = clients.findIndex(client => client.id === id);
 
     clients.splice(index, 1);
 
-    this.setState({ clients });
+    setState({ clients });
   };
 
-  handleAdd = client => {
-    const clients = [...this.state.clients];
+  const handleAdd = client => {
+    const clients = [...state.clients];
     clients.push(client);
 
-    this.setState({ clients });
+    setState({ clients });
   };
 
-  render() {
-    const title = "Liste des contacts";
+  const title = "Liste des contacts";
 
-    return (
-      <div className="p-10 text-center bg-blue-200">
-        <h1 className="text-3xl text-blue-900">{title}</h1>
-        <div className="flex justify-center">
-          <ul>
-            {this.state.clients.map(client => (
-              <Client
-                key={client.id}
-                details={client}
-                onDelete={this.handleDelete}
-              />
-            ))}
-          </ul>
-        </div>
-        <ClientForm onClientAdd={this.handleAdd} />
+  return (
+    <div className="p-10 text-center bg-blue-200">
+      <h1 className="text-3xl text-blue-900">{title}</h1>
+      <div className="flex justify-center">
+        <ul>
+          {state.clients.map(client => (
+            <Client
+              key={client.id}
+              details={client}
+              onDelete={handleDelete}
+            />
+          ))}
+        </ul>
       </div>
-    );
-  }
+      <ClientForm onClientAdd={handleAdd} />
+    </div>
+  );
 }
 
 export default ClientList
